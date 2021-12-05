e65494b0f2deb7cf50a98b66a9b3d65e6a3f534a
kouritron <kouritron@users.noreply.github.com>
2021-12-05 02:03:31 -0500

    Moved ReverseInt to D2 structure setup by dotnet, D1 is just hello mono now

---
+++ D1_MonoHello/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace HelloApp {
+    public class HelloWorld {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello Mono");
+            Console.WriteLine("System.Environment.Version: " + System.Environment.Version);
+            Console.WriteLine($"System.Environment.OSVersion: {System.Environment.OSVersion}");
+        }
+    }
+}
